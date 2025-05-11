3dc5270927278858e621873558d0c56b753c4431
britto <jose@britto.tech>
2025-05-11 15:40:59 -0400

    Make photography section dynamic

--- assets/js/loadData.js
+++ assets/js/loadData.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const art3d = document.getElementById('3d');
     const photos = document.getElementById('photos');
+    const footer = document.getElementById('footer');
 
     const htmlSnippet = `
     <div class="img">
@@ -37,6 +38,41 @@ document.addEventListener('DOMContentLoaded', () => {
                 const newElement = tempDiv.firstElementChild; // The <div class="img">
                 // Insert it before `photos` inside `art3d`
                 art3d.parentNode.insertBefore(newElement, photos);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching the file:', error);
+        });
+
+    fetch('assets/data/photography.txt')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to get photography.txt. Network response was ' + response.status + ' ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const lines = data.split('\n');
+            lines.forEach(line => {
+                if(!line || line.trim() === '') return; // Skip empty lines
+                let [alt, src] = line.split('|');
+                if (!alt || !src) {
+                    console.error('Invalid line format:', line);
+                    return;
+                }
+                alt = alt.trim();
+                src = src.trim();
+                if(alt === '' || src === '') {
+                    console.error('Empty alt or src:', line);
+                    return;
+                }
+                const imgHtml = htmlSnippet.replace('SRC', src).replaceAll('ALT', alt);
+                // Convert the string to a DOM node
+                const tempDiv = document.createElement('div');
+                tempDiv.innerHTML = imgHtml;
+                const newElement = tempDiv.firstElementChild; // The <div class="img">
+                // Insert it before `photos` inside `art3d`
+                photos.parentNode.insertBefore(newElement, footer);
             });
         })
         .catch(error => {
